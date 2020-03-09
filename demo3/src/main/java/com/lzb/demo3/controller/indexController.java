c6fe8f8692f5e1955ac408b563d47d7a7b3e3756
q6112618 <1403344050@qq.com>
2020-03-09 13:32:39 +0800

    cookie

--- demo3/src/main/java/com/lzb/demo3/controller/indexController.java
+++ demo3/src/main/java/com/lzb/demo3/controller/indexController.java
@@ -1,13 +1,32 @@
 package com.lzb.demo3.controller;
 
+import com.lzb.demo3.mapper.UserMapper;
+import com.lzb.demo3.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
 public class indexController {
+    @Autowired
+    UserMapper userMapper;
     @GetMapping("/")
-    public String hello() {
+    public String hello(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie:cookies){
+            if (cookie.getName().equals("token")){
+                String token=cookie.getValue();
+                User user=userMapper.findByToken(token);
+                if (user != null){
+                    request.getSession().setAttribute("gitHubUser", user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
--- demo3/src/main/java/com/lzb/demo3/mapper/UserMapper.java
+++ demo3/src/main/java/com/lzb/demo3/mapper/UserMapper.java
@@ -3,9 +3,12 @@ package com.lzb.demo3.mapper;
 import com.lzb.demo3.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     public void insert(User user);
+    @Select("select * from User where token=#{token}")
+    User findByToken(String token);
 }
